13f5f89e51c369cf2bfd4642dfa86fa90f83154f
Oscar <owiqvist@gmail.com>
2020-01-22 16:12:52 +0100

    Added glide and demo-image

--- app/src/main/java/com/example/sunflower/MyFragment.kt
+++ app/src/main/java/com/example/sunflower/MyFragment.kt
@@ -6,23 +6,31 @@ import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MyFragment : Fragment() {
 
     private var stepSize = 1
     private var counterToDisplay = 0
+    lateinit var testImage : ImageView
     lateinit var counter : TextView
     lateinit var incButton : Button
     lateinit var decButton : Button
     lateinit var chButton : Button
     lateinit var editText : EditText
     private var stepSizeKey = "step_size"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+            .load("https://i.imgur.com/Wk5rb6I.png")
+            .into(testImage)
+
+            //.into(test_image)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -37,7 +45,7 @@ class MyFragment : Fragment() {
         editText = view.findViewById<EditText>(R.id.edit_txt)
         editText.setText(stepSize.toString())
 
-
+        testImage = view.findViewById<ImageView>(R.id.test_image)
         counter = view.findViewById<TextView>(R.id.inc_num)
         chButton = view.findViewById<Button>(R.id.ch_button)
         incButton = view.findViewById<Button>(R.id.inc_button)
@@ -55,6 +63,7 @@ class MyFragment : Fragment() {
             stepSize = editText.text.toString().toInt()
             pref.edit().putInt(stepSizeKey, stepSize).commit()
         }
+
 
         return view
     }

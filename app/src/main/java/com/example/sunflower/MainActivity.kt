15ddb74a58170f43eeedf1929365043172cae30f
Oscar <owiqvist@gmail.com>
2020-01-23 13:57:28 +0100

    Added recycler for weather containers

--- app/src/main/java/com/example/sunflower/MainActivity.kt
+++ app/src/main/java/com/example/sunflower/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sunflower
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sunflower.list_example.ListFragment
+import com.example.sunflower.weather_list.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = ListFragment()
+        val fragment = WeatherListFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.frame, fragment)
         transaction.commit()
---
+++ app/src/main/java/com/example/sunflower/weather_list/WModel.kt
@@ -0,0 +1,4 @@
+package com.example.sunflower.weather_list
+
+data class WeatherReport(val location: String,
+                         val weather: String, val temp: Double, val weatherUrl: String)

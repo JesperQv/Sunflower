e9e01ce94a6c588adfc93c870c18f428d2177c7f
Oscar <owiqvist@gmail.com>
2020-01-24 14:56:44 +0100

    added data classes & interface

---
+++ app/src/main/java/com/example/sunflower/data/response/Coord.kt
@@ -0,0 +1,9 @@
+package com.example.sunflower.data.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
---
+++ app/src/main/java/com/example/sunflower/data/response/Wind.kt
@@ -0,0 +1,9 @@
+package com.example.sunflower.data.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Wind(
+    val deg: Int,
+    val speed: Double
+)
